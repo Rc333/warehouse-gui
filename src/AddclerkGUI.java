9518a3bdb120eb9e971b9aff452ecbc067efe150
bikos <riderstorm95@gmail.com>
2016-04-16 16:26:50 -0500

    some changes

--- src/AddclerkGUI.java
+++ src/AddclerkGUI.java
@@ -108,7 +108,7 @@ public class AddclerkGUI extends JFrame {
 		
 		JButton btnLogout = new JButton("Logout");
 		btnLogout.setIcon(new ImageIcon("C:\\Users\\bikra\\Desktop\\cancel.png"));
-		btnLogout.setBounds(10, 182, 99, 33);
+		btnLogout.setBounds(10, 182, 106, 33);
 		contentPane.add(btnLogout);
 		
 		
